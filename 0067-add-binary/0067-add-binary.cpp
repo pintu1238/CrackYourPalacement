694d2c15012ce41bb7611c1e5f51f2fa7af8c69d
Pintu Kumar <112333989+pintu1238@users.noreply.github.com>
2024-07-20 23:50:00 +0530

    Time: 9 ms (5.37%), Space: 12.3 MB (5.14%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int carry = 0;
+        string result = "";
+        int i = 0;
+        int alen = a.length();
+        int blen = b.length();
+        while(i < alen || i < blen || carry!= 0){
+            int x = 0;
+            if(i < alen && a[alen - 1 - i] == '1') x = 1;
+            int y = 0;
+            if(i < blen && b[blen - 1 - i] == '1') y = 1;
+            
+            result = to_string((x + y + carry) % 2) + result;
+            carry = (x + y + carry) / 2;
+            i++;
+        }
+        return result;
+    }
+};
